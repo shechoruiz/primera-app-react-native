f6a4b439a912c081bc95c3ce3bc75eab5be9adce
Sergio Andrés Ruiz Vélez <sergio.ruiz@carroya.com>
2022-05-02 15:41:26 -0500

    Creación de pantalla independiente

--- App.tsx
+++ App.tsx
@@ -1,14 +1,8 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import HolaMundoScreen from './src/screens/HolaMundoScreen';
 
 const App = () => {
-  return (
-    <View style={{flex: 1, justifyContent: 'center'}}>
-      <Text style={{fontSize: 45, textAlign: 'center', color: 'black'}}>
-        Hola señor
-      </Text>
-    </View>
-  );
+  return <HolaMundoScreen />;
 };
 
 export default App;
---
+++ src/screens/HolaMundoScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const HolaMundoScreen = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center'}}>
+      <Text style={{fontSize: 45, textAlign: 'center', color: 'black'}}>
+        Hola Sergio
+      </Text>
+    </View>
+  );
+};
+
+export default HolaMundoScreen;
