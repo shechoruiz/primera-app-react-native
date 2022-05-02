7e9dcbc1b08a266d15d063340688751cb0d49e57
Sergio Andrés Ruiz Vélez <sergio.ruiz@carroya.com>
2022-05-02 15:33:28 -0500

    Creación del proyecto - Hola Mundo

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center'}}>
+      <Text style={{fontSize: 45, textAlign: 'center', color: 'black'}}>
+        Hola señor
+      </Text>
+    </View>
+  );
+};
+
+export default App;
