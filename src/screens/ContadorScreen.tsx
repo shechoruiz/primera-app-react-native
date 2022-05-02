3b5a1442a2cab0e18eece56d34c8326ba2f332b1
Sergio Andrés Ruiz Vélez <sergio.ruiz@carroya.com>
2022-05-02 16:53:19 -0500

    Uso de API: StyleSheet

--- src/screens/ContadorScreen.tsx
+++ src/screens/ContadorScreen.tsx
@@ -1,23 +1,34 @@
 import React, {useState} from 'react';
-import {Text, View, TouchableOpacity} from 'react-native';
+import {Text, View, TouchableOpacity, StyleSheet} from 'react-native';
 
 const ContadorScreen = () => {
   const [contador, setContador] = useState(10);
 
   return (
-    <View style={{flex: 1, justifyContent: 'center'}}>
-      <Text
-        style={{fontSize: 40, textAlign: 'center', color: 'black', top: -15}}>
-        Contador: {contador}
-      </Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Contador: {contador}</Text>
       {/* <Button title="Click" onPress={() => setContador(contador + 1)} /> */}
       <TouchableOpacity onPress={() => setContador(contador + 1)}>
-        <View style={{borderRadius: 100, backgroundColor: 'red'}}>
+        <View style={styles.button}>
           <Text>+ 1</Text>
         </View>
       </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 40,
+    textAlign: 'center',
+    color: 'black',
+    top: -15,
+  },
+  button: {borderRadius: 100, backgroundColor: 'red'},
+});
 
 export default ContadorScreen;
