d99020adb84e3f0bf6ca41869f31c57fb6c4d9ea
Sergio Andrés Ruiz Vélez <sergio.ruiz@carroya.com>
2022-05-02 17:06:21 -0500

    Implementación de botón adicional

--- src/screens/ContadorScreen.tsx
+++ src/screens/ContadorScreen.tsx
@@ -8,9 +8,18 @@ const ContadorScreen = () => {
     <View style={styles.container}>
       <Text style={styles.title}>Contador: {contador}</Text>
       {/* <Button title="Click" onPress={() => setContador(contador + 1)} /> */}
-      <TouchableOpacity onPress={() => setContador(contador + 1)}>
-        <View style={styles.button}>
-          <Text>+ 1</Text>
+      <TouchableOpacity
+        onPress={() => setContador(contador - 1)}
+        style={styles.fabLocationBl}>
+        <View style={styles.fab}>
+          <Text style={styles.fabText}>- 1</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => setContador(contador + 1)}
+        style={styles.fabLocationBr}>
+        <View style={styles.fab}>
+          <Text style={styles.fabText}>+ 1</Text>
         </View>
       </TouchableOpacity>
     </View>
@@ -28,7 +37,29 @@ const styles = StyleSheet.create({
     color: 'black',
     top: -15,
   },
-  button: {borderRadius: 100, backgroundColor: 'red'},
+  fabLocationBr: {
+    position: 'absolute',
+    bottom: 25,
+    right: 25,
+  },
+  fabLocationBl: {
+    position: 'absolute',
+    bottom: 25,
+    left: 25,
+  },
+  fab: {
+    backgroundColor: '#5856D6',
+    width: 60,
+    height: 60,
+    borderRadius: 100,
+    justifyContent: 'center',
+  },
+  fabText: {
+    color: 'white',
+    fontSize: 25,
+    fontWeight: 'bold',
+    alignSelf: 'center',
+  },
 });
 
 export default ContadorScreen;
