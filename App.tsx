6322d0d29f8e8fd713d73f0b7cb9eb885a71c125
Sergio Andrés Ruiz Vélez <sergio.ruiz@carroya.com>
2022-05-05 22:44:14 -0500

    Fundamentos de diseño - Position

--- App.tsx
+++ App.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
 // import HolaMundoScreen from './src/screens/HolaMundoScreen';
-import ContadorScreen from './src/screens/ContadorScreen';
+// import ContadorScreen from './src/screens/ContadorScreen';
+// import BoxObjectModelScreen from './src/screens/BoxObjectModelScreen';
+// import DimensionesScreen from './src/screens/DimensionesScreen';
+import PosicionScreen from './src/screens/PosicionScreen';
 
 const App = () => {
   return (
-    // <HolaMundoScreen />
-    <ContadorScreen />
+    <SafeAreaView style={styles.view}>
+      {/* <HolaMundoScreen />
+      <ContadorScreen /> */}
+      {/* <BoxObjectModelScreen /> */}
+      {/* <DimensionesScreen /> */}
+      <PosicionScreen />
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  view: {
+    backgroundColor: '#A3B3C2',
+    flex: 1,
+  },
+});
 
 export default App;
---
+++ src/screens/PosicionScreen.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+
+const PosicionScreen = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.cajaVerde} />
+      <View style={styles.cajaMorada} />
+      <View style={styles.cajaNaranja} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    // flex: 1,
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    height: 400,
+    width: 400,
+    backgroundColor: '#28c4d9',
+  },
+  cajaMorada: {
+    width: 100,
+    height: 100,
+    backgroundColor: '#5856d6',
+    borderWidth: 10,
+    borderColor: 'white',
+    // top: 100,
+    // left: 100,
+    position: 'absolute',
+    top: 0,
+    right: 0,
+  },
+  cajaNaranja: {
+    width: 100,
+    height: 100,
+    backgroundColor: '#f0a23b',
+    borderWidth: 10,
+    borderColor: 'white',
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+  },
+  cajaVerde: {
+    // width: 100,
+    // height: 100,
+    backgroundColor: 'green',
+    borderWidth: 10,
+    borderColor: 'white',
+    // position: 'absolute',
+    // bottom: 0,
+    // left: 0,
+    // right: 0,
+    // top: 0,
+    ...StyleSheet.absoluteFillObject, // Este necesita que no se haya declarado ni un height ni un width para actuar
+  },
+});
+
+export default PosicionScreen;
