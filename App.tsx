9afa1f11d6dc34c6163e3bec7da4e66899a902b6
Sergio Andrés Ruiz Vélez <sergio.ruiz@carroya.com>
2022-05-07 16:38:39 -0500

    Resolución de tareas de diseño

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
-import FlexScreen from './src/screens/FlexScreen';
+// import FlexScreen from './src/screens/FlexScreen';
 // import HolaMundoScreen from './src/screens/HolaMundoScreen';
 // import ContadorScreen from './src/screens/ContadorScreen';
 // import BoxObjectModelScreen from './src/screens/BoxObjectModelScreen';
 // import DimensionesScreen from './src/screens/DimensionesScreen';
 // import PosicionScreen from './src/screens/PosicionScreen';
+import TareaScreen from './src/screens/TareaScreen';
 
 const App = () => {
   return (
@@ -15,14 +16,15 @@ const App = () => {
       {/* <BoxObjectModelScreen /> */}
       {/* <DimensionesScreen /> */}
       {/* <PosicionScreen /> */}
-      <FlexScreen />
+      {/* <FlexScreen /> */}
+      <TareaScreen />
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   view: {
-    backgroundColor: '#A3B3C2',
+    // backgroundColor: '#A3B3C2',
     flex: 1,
   },
 });
---
+++ src/screens/TareaScreen.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+
+const TareaScreen = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.cajaMorada} />
+      <View style={styles.cajaNaranja} />
+      <View style={styles.cajaCeleste} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#28425B',
+  },
+  cajaMorada: {
+    width: 100,
+    height: 100,
+    borderWidth: 10,
+    borderColor: 'white',
+    backgroundColor: '#5856d6',
+  },
+  cajaNaranja: {
+    width: 100,
+    height: 100,
+    borderWidth: 10,
+    borderColor: 'white',
+    backgroundColor: '#f0a23b',
+  },
+  cajaCeleste: {
+    width: 100,
+    height: 100,
+    borderWidth: 10,
+    borderColor: 'white',
+    backgroundColor: '#28c4d9',
+  },
+});
+
+export default TareaScreen;
